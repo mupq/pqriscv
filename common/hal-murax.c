003664c636603a61434b086779190228ce16b4f4
Richard Petri <git@rpls.de>
2019-05-11 21:58:24 +0200

    Adapt Makefile system from PQM4 and create HAL stubs for Murax

---
+++ common/hal-murax.c
@@ -0,0 +1,13 @@
+#include "hal.h"
+
+void hal_setup(const enum clock_mode clock)
+{
+}
+
+void hal_send_str(const char* in)
+{
+}
+
+uint64_t hal_get_time(void)
+{
+}
--- interface.py
+++ interface.py
@@ -1,30 +1,40 @@
-import subprocess
-
 from mupq import mupq
 
-
-class RISCVSettings(mupq.PlatformSettings):
+class MuraxSettings(mupq.PlatformSettings):
     #: Specify folders to include
     scheme_folders = [  # mupq.PlatformSettings.scheme_folders + [
-        ('pqm4', 'crypto_kem'),
-        ('pqm4', 'crypto_sign'),
+        ('pqriscv', 'crypto_kem', ''),
+        ('pqriscv', 'crypto_sign', ''),
+        ('mupq', 'mupq/crypto_kem', ''),
+        ('mupq', 'mupq/crypto_sign', ''),
+        ('pqclean', 'mupq/pqclean/crypto_kem', "PQCLEAN"),
+        ('pqclean', 'mupq/pqclean/crypto_sign', "PQCLEAN")
+
     ]
 
     #: List of dicts, in each dict specify (Scheme class) attributes of the
     #: scheme with values, if all attributes match the scheme is skipped.
     skip_list = (
-        {'scheme': 'frodo640-aes', 'implementation': 'ref'},
-        {'scheme': 'frodo640-cshake', 'implementation': 'ref'},
+        {'scheme': 'frodokem640aes', 'implementation': 'clean'},
+        {'scheme': 'frodokem976aes', 'implementation': 'clean'},
+        {'scheme': 'frodokem1344aes', 'implementation': 'clean'}
     )
 
 
-class RISCV(mupq.Platform):
+class Murax(mupq.Platform):
 
-    # return device for serial communication
+    def __enter__(self):
+        # TODO: Implement something
+        return super().__enter__()
+
+    def __exit__(self,*args, **kwargs):
+        # TODO: Implement something
+        return super().__exit__(*args, **kwargs)
+
     def device(self):
-        raise NotImplementedError
+        # TODO: Implement something
+        return None
 
-    # flash binary to device
     def flash(self, binary_path):
-        raise NotImplementedError
-
+        super().flash(binary_path)
+        # TODO: Implement something
