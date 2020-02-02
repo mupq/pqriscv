35f9de8e4c701cc1190396adb4161c87d1cb7bea
Richard Petri <git@rpls.de>
2020-02-02 19:24:34 +0100

    Add testvector test

--- testvectors.py
+++ testvectors.py
@@ -3,10 +3,9 @@ from mupq import mupq
 from interface import *
 
 import sys
-
 if __name__ == "__main__":
     args, rest = parse_arguments()
     riscv, settings = get_platform(args)
     with riscv:
         test = mupq.TestVectors(settings, riscv)
-        test.test_all(sys.argv[1:])
+        test.test_all(rest)
